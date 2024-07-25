9dc79951ddba1c1f23c2e8aff7df01b4726ef880
Luque <lschnoller@gmail.com>
2024-07-25 12:29:51 -0300

    about us update

---
+++ src/app/components/AboutUs.tsx
@@ -0,0 +1,42 @@
+const AboutUs = () => (
+    <section id="about-us" className="py-10 px-5 text-gray-100">
+        <div className="container mx-auto">
+            <h2 className="text-3xl font-bold mb-5">About Us</h2>
+            <p className="mb-4">
+                Welcome to HelpOneAnother.net! We are a passionate startup based in Brazil, dedicated to solving accounting and finance challenges for families and startups. Our mission is to empower individuals and small businesses by providing innovative solutions that simplify financial management and promote economic growth.
+            </p>
+            <p className="mb-4">
+                At HelpOneAnother, we understand the complexities of managing finances in today's fast-paced world. Our team of experts is committed to developing user-friendly tools and services that make financial administration easier and more accessible. Whether you're looking to streamline your personal budget, manage your family's finances, or optimize your startup's accounting processes, we have the solutions you need.
+            </p>
+            <p className="mb-4">
+                We believe that knowledge is power, and by offering insightful financial guidance and cutting-edge technology, we aim to help you achieve financial stability and success. Join us on this journey and let's help one another thrive financially!
+            </p>
+            <p>
+                Explore our services and discover how HelpOneAnother.net can make a difference in your financial life today.
+            </p>
+            <h3 className="text-1xl font-bold mt-10 mb-2">Founders</h3>
+            <ul>
+                <li className="mb-3 flex items-center">
+                    <img src="https://media.licdn.com/dms/image/C4E03AQGSOdCSaXq2gQ/profile-displayphoto-shrink_400_400/0/1575487961789?e=1727308800&v=beta&t=AR8RZGnmJ4mUH-znx8knbQReA-st9q4ci9YQRxwrZlc" alt="Lucas Schnoller" className="w-5 h-5 rounded-full mr-2" />
+                    <a href="https://www.linkedin.com/in/lschnoller" className="hover:underline">
+                        Lucas Schnoller
+                    </a>
+                </li>
+                <li className="mb-3 flex items-center">
+                    <img src="https://media.licdn.com/dms/image/D4D03AQG-2-0jVkxTTg/profile-displayphoto-shrink_800_800/0/1701438495425?e=1727308800&v=beta&t=1RCc-VonQSKPGds2SdMobJQhnY_lbayOumbSDtGxKX4" alt="Eduardo Santos" className="w-5 h-5 rounded-full mr-2" />
+                    <a href="https://www.linkedin.com/in/eduardo~santos/" className="hover:underline">
+                        Eduardo Santos
+                    </a>
+                </li>
+                <li className="mb-3 flex items-center">
+                    <img src="https://media.licdn.com/dms/image/C4D03AQE3MI_6BLAAHA/profile-displayphoto-shrink_400_400/0/1607717129038?e=1727308800&v=beta&t=4yiPPwDLjiuii9nxDmEtZHloCKLJxalFqCVQ2Q-Eaus" alt="Bianca Steffen Zen" className="w-5 h-5 rounded-full mr-2" />
+                    <a href="https://www.linkedin.com/in/bianca-steffen-zen-6b311852/" className="hover:underline">
+                        Bianca Steffen Zen
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+);
+
+export default AboutUs;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,11 @@ import * as AiIcons from 'react-icons/ai';
 //import Icon from '@ant-design/icons';
 import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
+import AboutUs from './components/AboutUs';
 
 export default function Home() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [showAboutUs, setShowAboutUs] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -23,6 +25,14 @@ export default function Home() {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
+  const handleNavClick = (section: string) => {
+    if (section === 'about') {
+      setShowAboutUs(true);
+    } else {
+      setShowAboutUs(false);
+    }
+  };
 
   return (
     <div className="w-full min-h-screen mx-auto bg-gradient-to-br from-sky-blue-light to-sky-blue-dark">
@@ -63,10 +73,10 @@ export default function Home() {
         <nav>
           <ul className="flex gap-5">
             <li>
-              <a href="#services" className="text-white hover:underline">Services</a>
+              <a href="#services" className="text-white hover:underline" onClick={() => handleNavClick('services')}>Services</a>
             </li>
             <li>
-              <a href="#" className="text-white hover:underline">About</a>
+              <a href="#about-us" className="text-white hover:underline" onClick={() => handleNavClick('about')}>About</a>
             </li>
             <li>
               <a href="https://calendly.com/lschnoller/free-consultation" className="text-white hover:underline">Contact</a>
@@ -77,73 +87,79 @@ export default function Home() {
       {/* separator */}
       <div className="h-16 md:h-20"></div>
       <main className="flex flex-col min-h-screen justify-center items-center md:px-40 px-20 pt-40 2xl:pt-40">
-        <section className="mb-28 2xl:mb-40">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 3 }}
-          >
+        {!showAboutUs ? (
+          <>
+            <section className="mb-28 2xl:mb-40">
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 3 }}
+              >
 
-            <h1 className="text-3xl 2xl:text-4xl font-bold text-center text-white mb-7">
-              Import your bank statements. Let us do the rest.
-            </h1>
-            <p className="text-xl text-center text-gray-200 mb-20">
-              Stop spending your time analyzing your results. We use the latest <u>AI technology</u> to process your bank statements and give you full details about your finances and results.
-            </p>
-            <div className='mx-auto block text-center'>
-              <button className="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-pink-500 hover:to-purple-500 text-white font-bold py-3 px-6 rounded-full">
-                <a href="https://calendly.com/lschnoller/free-consultation">Get Free Consultation</a>
-              </button>
-            </div>
-          </motion.div>
-        </section>
-        <section id="services" className="mt-30 mb-56 max-w-7xl">
-          {/* <h2 className="text-2xl font-semibold text-center text-white mb-8">Our Key Services</h2> */}
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-x-10 gap-y-8">
-            <div className="flex flex-col items-center">
-              <AiIcons.AiOutlineFund className="text-4xl text-white mb-4" />
-              <h3 className="text-lg font-medium text-white mb-2 text-center">AI Automated Financial Results</h3>
-              <p className="text-gray-300 text-center leading-snug">
-                Simple. Get a clear idea of your results just by importing your bank statements.
-              </p>
-            </div>
-            <div className="flex flex-col items-center">
-              <AiIcons.AiOutlineGlobal className="text-4xl text-white mb-4" />
-              <h3 className="text-lg font-medium text-white mb-2 text-center">All Resources in One Place</h3>
-              <p className="text-gray-300 text-center">
-                Get a complete overview: bank accounts, taxes, investments, loans, etc.
-              </p>
-            </div>
-            <div className="flex flex-col items-center">
-              <AiIcons.AiOutlineSafety className="text-4xl text-white mb-4" />
-              <h3 className="text-lg font-medium text-white mb-2 text-center">Private by Design</h3>
-              <p className="text-gray-300 text-center">
-                Our system anonymizes all of your data, keeping it private by design.
-              </p>
-            </div>
-            <div className="flex flex-col items-center">
-              <AiIcons.AiOutlineCreditCard className="text-4xl text-white mb-4" />
-              <h3 className="text-lg font-medium text-white mb-2 text-center">Accounts Payable</h3>
-              <p className="text-gray-300 text-center">
-                Never be late for a payment again. Manage your bills with ease.
-              </p>
-            </div>
-            <div className="flex flex-col items-center">
-              <AiIcons.AiOutlineDollar className="text-4xl text-white mb-4" />
-              <h3 className="text-lg font-medium text-white mb-2 text-center">Accounts Receivable</h3>
-              <p className="text-gray-300 text-center">
-                Add your service contracts and let us follow-up with your clients&apos; payments.
-              </p>
-            </div>
-            <div className="flex flex-col items-center">
-              <AiIcons.AiOutlineUsergroupAdd className="text-4xl text-white mb-4" />
-              <h3 className="text-lg font-medium text-white mb-2 text-center">Share with your Accountant</h3>
-              <p className="text-gray-300 text-center">
-                Allow access to your accountant for accounting services.
-              </p>
-            </div>
-          </div>
-        </section>
+                <h1 className="text-3xl 2xl:text-4xl font-bold text-center text-white mb-7">
+                  Import your bank statements. Let us do the rest.
+                </h1>
+                <p className="text-xl text-center text-gray-200 mb-20">
+                  Stop spending your time analyzing your results. We use the latest <u>AI technology</u> to process your bank statements and give you full details about your finances and results.
+                </p>
+                <div className='mx-auto block text-center'>
+                  <button className="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-pink-500 hover:to-purple-500 text-white font-bold py-3 px-6 rounded-full">
+                    <a href="https://calendly.com/lschnoller/free-consultation">Get Free Consultation</a>
+                  </button>
+                </div>
+              </motion.div>
+            </section>
+            <section id="services" className="mt-30 mb-56 max-w-7xl">
+              {/* <h2 className="text-2xl font-semibold text-center text-white mb-8">Our Key Services</h2> */}
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-x-10 gap-y-8">
+                <div className="flex flex-col items-center">
+                  <AiIcons.AiOutlineFund className="text-4xl text-white mb-4" />
+                  <h3 className="text-lg font-medium text-white mb-2 text-center">AI Automated Financial Results</h3>
+                  <p className="text-gray-300 text-center leading-snug">
+                    Simple. Get a clear idea of your results just by importing your bank statements.
+                  </p>
+                </div>
+                <div className="flex flex-col items-center">
+                  <AiIcons.AiOutlineGlobal className="text-4xl text-white mb-4" />
+                  <h3 className="text-lg font-medium text-white mb-2 text-center">All Resources in One Place</h3>
+                  <p className="text-gray-300 text-center">
+                    Get a complete overview: bank accounts, taxes, investments, loans, etc.
+                  </p>
+                </div>
+                <div className="flex flex-col items-center">
+                  <AiIcons.AiOutlineSafety className="text-4xl text-white mb-4" />
+                  <h3 className="text-lg font-medium text-white mb-2 text-center">Private by Design</h3>
+                  <p className="text-gray-300 text-center">
+                    Our system anonymizes all of your data, keeping it private by design.
+                  </p>
+                </div>
+                <div className="flex flex-col items-center">
+                  <AiIcons.AiOutlineCreditCard className="text-4xl text-white mb-4" />
+                  <h3 className="text-lg font-medium text-white mb-2 text-center">Accounts Payable</h3>
+                  <p className="text-gray-300 text-center">
+                    Never be late for a payment again. Manage your bills with ease.
+                  </p>
+                </div>
+                <div className="flex flex-col items-center">
+                  <AiIcons.AiOutlineDollar className="text-4xl text-white mb-4" />
+                  <h3 className="text-lg font-medium text-white mb-2 text-center">Accounts Receivable</h3>
+                  <p className="text-gray-300 text-center">
+                    Add your service contracts and let us follow-up with your clients&apos; payments.
+                  </p>
+                </div>
+                <div className="flex flex-col items-center">
+                  <AiIcons.AiOutlineUsergroupAdd className="text-4xl text-white mb-4" />
+                  <h3 className="text-lg font-medium text-white mb-2 text-center">Share with your Accountant</h3>
+                  <p className="text-gray-300 text-center">
+                    Allow access to your accountant for accounting services.
+                  </p>
+                </div>
+              </div>
+            </section>
+          </>
+        ) : (
+          <AboutUs />
+        )}
       </main>
       <footer>
 
